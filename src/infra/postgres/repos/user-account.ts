72ac04d06ee4ef4abf17786c845f59cefa6b7f08
Alex Ferreira Santos <alexfstos@gmail.com>
2023-12-03 16:13:21 -0300

    make PgUserAccountRepository implement SaveUserAccountRepository interface

--- src/infra/postgres/repos/user-account.ts
+++ src/infra/postgres/repos/user-account.ts
@@ -10,7 +10,8 @@ type LoadResult = LoadUserAccountRepository.Result
 type SaveParams = SaveFacebookAccountRepository.Params
 type SaveResult = SaveFacebookAccountRepository.Result
 
-export class PgUserAccountRepository implements LoadUserAccountRepository {
+export class PgUserAccountRepository
+  implements LoadUserAccountRepository, SaveFacebookAccountRepository {
   private readonly pgUserRepo = getRepository(PgUser)
 
   async load(params: LoadParams): Promise<LoadResult> {
