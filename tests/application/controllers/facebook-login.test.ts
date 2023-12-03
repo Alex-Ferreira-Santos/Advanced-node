f94f0d189736140ff21f2f017958d851441c4fb7
Alex Ferreira Santos <alexfstos@gmail.com>
2023-12-03 18:46:12 -0300

    apply unauthorized helper

--- tests/application/controllers/facebook-login.test.ts
+++ tests/application/controllers/facebook-login.test.ts
@@ -3,7 +3,7 @@ import { FacebookAuthentication } from '@/domain/features'
 import { AccessToken } from '@/domain/models'
 import { MockProxy, mock } from 'jest-mock-extended'
 import { FacebookLoginController } from '@/application/controllers'
-import { RequiredFieldError, ServerError } from '@/application/errors'
+import { RequiredFieldError, ServerError, UnauthorizedError } from '@/application/errors'
 
 describe('FacebookLoginController', () => {
   let sut: FacebookLoginController
@@ -54,7 +54,7 @@ describe('FacebookLoginController', () => {
     const httpResponse = await sut.handle({ token: 'any_token' })
     expect(httpResponse).toEqual({
       statusCode: 401,
-      data: new AuthenticationError()
+      data: new UnauthorizedError()
     })
   })
 
